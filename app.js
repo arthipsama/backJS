a59fa3cf527de0efb440c85e90bc3c81e23aa7c1
Arthip Srain <117334939+arthipsama@users.noreply.github.com>
2024-01-20 23:30:24 +0700

    ฟหกฟหกฟหกฟหกฟหก

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const roomRouter = require('./room');  // เปลี่ยนตามชื่อไฟล์ที่คุณได้ตั้งในโปรเจคของคุณ
+const roomRouter = require('./room'); 
+
+const corsOptions = {
+  origin: 'http://localhost:4200',
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions)); // ให้ใช้ cors ก่อนการใช้ Router
 
 app.use(express.static('public'));
-
-// เรียกใช้ Router สำหรับ API
 app.use('/api', roomRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
-const corsOptions = {
-  origin: 'http://localhost:4200', // หรือวาง origin ของเว็บแอป Angular ของคุณที่นี่
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
