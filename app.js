42e12172e9287f61715e8e7bf5f2ad58d2ed98e7
Arthip Srain <117334939+arthipsama@users.noreply.github.com>
2024-01-20 22:54:10 +0700

    asdasd

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cors());
 // ตรวจสอบว่าคุณได้ให้บริการเส้นทาง '/api/room' หรือไม่
 app.use('/api', roomRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
