76525b85ba825644dd9d742ca2d6e45e9a6c927c
Arthip Srain <117334939+arthipsama@users.noreply.github.com>
2024-01-20 23:02:55 +0700

    ฟหกฟหกฟห

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-// backend/app.js (หรือ server.js)
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const roomRouter = require('./room'); // ตรวจสอบชื่อไฟล์ที่สามารถเป็นไปได้
-
 const app = express();
+const roomRouter = require('./room');  // เปลี่ยนตามชื่อไฟล์ที่คุณได้ตั้งในโปรเจคของคุณ
+
 app.use(express.static('public'));
 
-app.use(bodyParser.json());
-app.use(cors());
-
-// ตรวจสอบว่าคุณได้ให้บริการเส้นทาง '/api/room' หรือไม่
+// เรียกใช้ Router สำหรับ API
 app.use('/api', roomRouter);
 
-const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
